81d6ec3311c743cda68f5ecc952918db6568fd98
Evan Hochstein <ehochstein27@gmail.com>
2020-11-14 23:31:58 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('AWS Testing'));
+app.get('/', (req, res) => res.send('AWS Testing again with more information!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
